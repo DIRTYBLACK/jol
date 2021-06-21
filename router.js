3905faed9a51427e8a95ca5f1c2df6d796a35f93
DIRTYBLACK <58027343+DIRTYBLACK@users.noreply.github.com>
2021-06-21 12:21:33 +0900

    Update router.js

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 module.exports = (app) => {  
   const devices = require('./device.controller.js');  //contact.controller.js를 로딩
-  app.get('/jdevices', devices.findAll);             //모든 연락처 검색
-  app.get('/jdevices/:deviceId', devices.findOne);  //특정 연락처 검색
-  app.post('/jdevices',devices.create);             //새로운 연락처 추가
-  app.put('/jdevices/:deviceId', devices.update);   //특정 연락처 업데이트
-  app.delete('/jdevices/:deviceId', devices.delete); //특정 연락처 삭제  
+  app.get('/jdevices', devices.findAll);             //모든 기기 검색
+  app.get('/jdevices/:deviceId', devices.findOne);  //특정 기기 검색
+  app.post('/jdevices',devices.create);             //새로운 기기 추가
+  app.put('/jdevices/:deviceId', devices.update);   //특정 기기 업데이트
+  app.delete('/jdevices/:deviceId', devices.delete); //기기 데이터 삭제  
 
   const jdatas = require('./jdata.controller.js');  //contact.controller.js를 로딩
   app.get('/jdatas', jdatas.findAll);            //데이터 표시
